6e775dc67a7e6758edc66b31667ff300aef0de79
nnagornyy <n.nagornyy@gmail.com>
2017-11-25 15:51:10 +0300

    xdebug support added

--- .vagrant/update.sh
+++ .vagrant/update.sh
@@ -8,11 +8,28 @@
 #yum remove -y php-pdo
 #yum install -y php-pdo
 
+echo '
+██████╗ ██╗████████╗██████╗ ██╗██╗  ██╗    ██╗   ██╗███╗   ███╗
+██╔══██╗██║╚══██╔══╝██╔══██╗██║╚██╗██╔╝    ██║   ██║████╗ ████║
+██████╔╝██║   ██║   ██████╔╝██║ ╚███╔╝     ██║   ██║██╔████╔██║
+██╔══██╗██║   ██║   ██╔══██╗██║ ██╔██╗     ╚██╗ ██╔╝██║╚██╔╝██║
+██████╔╝██║   ██║   ██║  ██║██║██╔╝ ██╗     ╚████╔╝ ██║ ╚═╝ ██║
+╚═════╝ ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝      ╚═══╝  ╚═╝     ╚═╝
+
+
+===============================================================
+
+'
+
+echo -e "\e[1;32mSetting XDebug !\e[0m"
 rm -f /etc/php.d/*xdebug.ini*
 
 echo 'zend_extension = xdebug.so
-xdebug.remote_enable = On
-xdebug.remote_connect_back = On' >> /etc/php.d/15-xdebug.ini.disabled
+xdebug.remote_enable = true
+xdebug.remote_autostart = true
+xdebug.idekey = "PHP_STORM"
+xdebug.remote_host=192.168.33.1
+xdebug.remote_handler = dbgp' >> /etc/php.d/15-xdebug.ini.disabled
 
 mv -f /etc/php.d/15-xdebug.ini.disabled /etc/php.d/15-xdebug.ini
 
@@ -21,11 +38,11 @@ service httpd restart
 
 mkdir -p /tmp/php_sessions/www
 mkdir -p /tmp/php_upload/www
-wget --quiet --directory-prefix=/home/bitrix/www http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php
 
-chown -R bitrix:bitrix /home/bitrix
-chown -R bitrix:bitrix /tmp/php_sessions
-chown -R bitrix:bitrix /tmp/php_upload
+echo -e "\e[1;32mDownload bitrix fast start script\e[0m"
+wget --quiet --directory-prefix=/home/bitrix/www http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php
+wget --quiet --directory-prefix=/home/bitrix/www http://www.1c-bitrix.ru/download/scripts/restore.php 
+
 
 echo '
 EnableSendfile Off
@@ -34,3 +51,5 @@ EnableSendfile Off
 echo '
 sendfile off;
 ' >> /etc/nginx/bx/conf/bitrix_general.conf
+
+echo -e "\e[1;32mAll done, enjoy!\e[0m"
